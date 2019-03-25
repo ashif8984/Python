7d5e556d7656e49a5fb11bbafcd12e02d1fdbe85
ashif <ashif8984@gmail.com>
2019-03-25 19:01:13 +0530

    Added commments in selenium_test.py file

--- selenium_test.py
+++ selenium_test.py
@@ -1,3 +1,15 @@
+'''
+This Script does the selenium tests for a webpage and click link
+
+Prerequites:
+    1. Install python and pip module
+    2. Set the python enviornment variable
+    3. Install selenium (pip install selenium)
+    4. Install geckodriver for firefox browser & paste the geckdriver.exe under Script folder of Python
+
+'''
+
+# importing selenium modules
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -10,9 +22,12 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
 
+#WebPage to Test
 driver.get("http://54.71.52.140/")
+
+#Selecting the Element to Test(here href element)
 element =driver.find_element_by_link_text('About Us')
-driver.implicitly_wait(5)
+driver.implicitly_wait(5) #wait for 5 seconds
 
 element.click()
 

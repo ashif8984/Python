efc05cff5834ffceb0b635a50977d63c69fa9207
= <ashif8984@gmail.com>
2020-03-09 01:16:58 +0530

    Added logic to pattern

---
+++ pattern_print.py
@@ -0,0 +1,6 @@
+# 10 is the total number to print
+for num in range(5):
+    for i in range(num):
+        print ("*", end=" ") #print Pattern
+    # new line after each row to display pattern correctly
+    print("\n")
